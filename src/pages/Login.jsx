d923e07447fc7ca45d7c34963399d75fb8ae8fee
Gabriel <gabrielferreiradsl@hotmail.com>
2021-11-05 16:31:20 -0300

    requisito 3, 4 e 5

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import RecipesContext from '../context/RecipesContext';
 
 function Login() {
+  const [emailAndPassword, setEmailAndPassword] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = ({ target: { name, value } }) => {
+    setEmailAndPassword((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const validateEmail = (email) => {
+    const regexMail = /\S+@\S+\.\S+/;
+    return regexMail.test(email);
+  };
+
+  const validateLoginBtn = () => {
+    const NUMBER_SIX = 6;
+    return !(emailAndPassword.password.length > NUMBER_SIX
+        && validateEmail(emailAndPassword.email));
+  };
+
   return (
     <main>
       <h3>Login</h3>
-      <input placeholder="Email" data-testid="email-input" type="text" name="" id="" />
-      <input placeholder="Senha" data-testid="password-input" type="text" name="" id="" />
-      <button data-testid="login-submit-btn" type="button">Entrar</button>
+      <input
+        name="email"
+        value={ emailAndPassword.email }
+        placeholder="Email"
+        data-testid="email-input"
+        type="text"
+        onChange={ (ev) => handleChange(ev) }
+      />
+      <input
+        name="password"
+        value={ emailAndPassword.password }
+        placeholder="Senha"
+        data-testid="password-input"
+        type="text"
+        onChange={ (ev) => handleChange(ev) }
+      />
+      <button
+        disabled={ validateLoginBtn() }
+        data-testid="login-submit-btn"
+        type="button"
+      >
+        Entrar
+      </button>
     </main>
   );
 }
