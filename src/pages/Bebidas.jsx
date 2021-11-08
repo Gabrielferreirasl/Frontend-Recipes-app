de8d84b1a15afd281274e59f3e249df7218c1afb
Gabriel <gabrielferreiradsl@hotmail.com>
2021-11-08 15:01:26 -0300

    requisito 16

--- src/pages/Bebidas.jsx
+++ src/pages/Bebidas.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
+import RecipesContext from '../context/RecipesContext';
 
 function Bebidas() {
+  const { recipes: { drinks } } = useContext(RecipesContext);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (drinks.length === 1) {
+      history.push(`bebidas/${drinks[0].idDrink}`);
+    }
+  }, [drinks, history]);
+
   return (
     <main>
       <Header />
--- src/pages/Comidas.jsx
+++ src/pages/Comidas.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
+import RecipesContext from '../context/RecipesContext';
 
 function Comidas() {
+  const { recipes: { meals } } = useContext(RecipesContext);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (meals.length === 1) {
+      history.push(`comidas/${meals[0].idMeal}`);
+    }
+  }, [meals, history]);
+
   return (
     <main>
       <Header />
