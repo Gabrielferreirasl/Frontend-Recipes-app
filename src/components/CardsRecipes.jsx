1229993c9cb1244628816cdaf4cb503b6c78c2a9
Matheus Alves <malves224@gmail.com>
2021-11-09 16:11:33 -0300

    fix lint

--- src/components/CardsRecipes.jsx
+++ src/components/CardsRecipes.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import { useHistory, Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 function CardsRecipes({ items, maxItems }) {
