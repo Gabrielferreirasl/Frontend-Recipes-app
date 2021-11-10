845b3a8ef1c07a2c49c72d88cfd7aa7f68486194
Cristhyane Araldi <cristhyane.araldi@hotmail.com>
2021-11-09 21:47:00 -0300

    Requisitos 82 a 87

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import profileIcon from '../images/profileIcon.svg';
 
@@ -20,16 +19,21 @@ function Perfil({ history }) {
       </header>
       <div>
         <h3 data-testid="profile-email">{ emailUser.email }</h3>
-        <Link to="/receitas-feitas">
-          <button type="button" data-testid="profile-done-btn">
-            Receitas Feitas
-          </button>
-        </Link>
-        <Link to="/receitas-favoritas">
-          <button type="button" data-testid="profile-favorite-btn">
-            Receitas Favoritas
-          </button>
-        </Link>
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+          onClick={ () => history.push('/receitas-feitas') }
+        >
+          Receitas Feitas
+        </button>
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+          onClick={ () => history.push('/receitas-favoritas') }
+        >
+          Receitas Favoritas
+        </button>
+
         <button type="button" data-testid="profile-logout-btn" onClick={ handleClick }>
           Sair
         </button>
