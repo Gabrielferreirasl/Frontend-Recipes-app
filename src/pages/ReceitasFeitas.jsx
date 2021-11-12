eb7c35271980a2aeec59ef936e2fa07fc7d244cc
Cristhyane Araldi <cristhyane.araldi@hotmail.com>
2021-11-12 10:21:00 -0300

    Requisito 58

--- src/pages/ReceitasFeitas.jsx
+++ src/pages/ReceitasFeitas.jsx
@@ -1,14 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import CardToFavoriteOrDone from '../components/CardToFavoriteOrDone';
-import { getLocalStorage } from '../helpers';
+import FiltersByType from '../components/FiltersByType';
+import { getLocalStorage, updateRecipes } from '../helpers';
 import profileIcon from '../images/profileIcon.svg';
 
 function ReceitasFeitas() {
   const [doneRecipes, setDoneRecipes] = useState([]);
+  const [filters, setFilters] = useState({
+    type: 'all',
+  });
 
   useEffect(() => {
-    setDoneRecipes(getLocalStorage('doneRecipes'));
-  }, []);
+    updateRecipes('doneRecipes', filters.type, setDoneRecipes);
+  }, [filters.type]);
 
   return (
     <main>
@@ -17,7 +21,7 @@ function ReceitasFeitas() {
         <h2 data-testid="page-title">Receitas Feitas</h2>
       </header>
       <span>
-        <button type="button" data-testid="filter-by-all-btn">
+        {/* <button type="button" data-testid="filter-by-all-btn">
           All
         </button>
         <button type="button" data-testid="filter-by-food-btn">
@@ -25,7 +29,8 @@ function ReceitasFeitas() {
         </button>
         <button type="button" data-testid="filter-by-drink-btn">
           Drinks
-        </button>
+        </button> */}
+        <FiltersByType filters={ filters } setState={ setFilters } />
       </span>
       <div>
         { doneRecipes && doneRecipes.map((recipe, index) => (
@@ -33,7 +38,7 @@ function ReceitasFeitas() {
             key={ recipe.id }
             item={ recipe }
             index={ index }
-            favoriteOrDone="done"
+            favoriteOrMade="done"
           />
 
         )) }
