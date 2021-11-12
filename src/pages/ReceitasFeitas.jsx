c252493bad6b181ced2e04701f4af47774dcb870
Cristhyane Araldi <cristhyane.araldi@hotmail.com>
2021-11-12 10:08:02 -0300

    Requisito 56

--- src/pages/ReceitasFeitas.jsx
+++ src/pages/ReceitasFeitas.jsx
@@ -1,14 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import CardToFavoriteOrDone from '../components/CardToFavoriteOrDone';
+import { getLocalStorage } from '../helpers';
 import profileIcon from '../images/profileIcon.svg';
-import shareIcon from '../images/shareIcon.svg';
 
 function ReceitasFeitas() {
   const [doneRecipes, setDoneRecipes] = useState([]);
 
   useEffect(() => {
-    const getRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
-    setDoneRecipes(getRecipes);
+    setDoneRecipes(getLocalStorage('doneRecipes'));
   }, []);
 
   return (
@@ -30,23 +29,12 @@ function ReceitasFeitas() {
       </span>
       <div>
         { doneRecipes && doneRecipes.map((recipe, index) => (
-          <div key={ index }>
-            <Link
-              to={ recipe.type === 'comida'
-                ? `/comidas/${recipe.id}` : `/bebidas/${recipe.id}` }
-            >
-              <img
-                src={ recipe.image }
-                alt="recipe"
-                data-testid={ `${index}-horizontal-image` }
-              />
-              <h3 data-testid={ `${index}-horizontal-name` }>Nome da receita</h3>
-            </Link>
-            <p data-testid={ `${index}-horizontal-top-text` }>Categoria da receita</p>
-            <p data-testid={ `${index}-horizontal-done-date` }>Data em que foi feita</p>
-            <p data-testid={ `${index}-${recipe.tag}-horizontal-tag` }>Tags da receita</p>
-            <img src={ shareIcon } alt="shareIcon" data-testid={ `${index}-horizontal-share-btn` } />
-          </div>
+          <CardToFavoriteOrDone
+            key={ recipe.id }
+            item={ recipe }
+            index={ index }
+            favoriteOrDone="done"
+          />
 
         )) }
       </div>
