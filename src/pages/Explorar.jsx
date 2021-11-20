0df7d33e32d99ab1ff89baf09499914148353d2a
Matheus Alves <malves224@gmail.com>
2021-11-20 11:42:24 -0300

    estilizaçao tela de explorar

--- src/pages/Explorar.jsx
+++ src/pages/Explorar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, useHistory } from 'react-router-dom';
+import '../style/explorar.css';
 import Footer from '../components/Footer';
 import profileIcon from '../images/profileIcon.svg';
 
@@ -14,20 +15,22 @@ function Explorar() {
         </Link>
         <h2 data-testid="page-title">Explorar</h2>
       </header>
-      <button
-        onClick={ () => history.push('/explorar/comidas') }
-        data-testid="explore-food"
-        type="button"
-      >
-        Explorar Comidas
-      </button>
-      <button
-        onClick={ () => history.push('/explorar/bebidas') }
-        data-testid="explore-drinks"
-        type="button"
-      >
-        Explorar Bebidas
-      </button>
+      <div className="container-explorar">
+        <button
+          onClick={ () => history.push('/explorar/comidas') }
+          data-testid="explore-food"
+          type="button"
+        >
+          Explorar Comidas
+        </button>
+        <button
+          onClick={ () => history.push('/explorar/bebidas') }
+          data-testid="explore-drinks"
+          type="button"
+        >
+          Explorar Bebidas
+        </button>
+      </div>
       <Footer />
     </main>
   );
