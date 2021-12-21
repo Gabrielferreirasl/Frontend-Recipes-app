abf888f05882cfe74576570cad5dd80f948af7ba
Gabriel <gabrielferreiradsl@hotmail.com>
2021-12-20 21:04:32 -0300

    vercel enviroment added

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <BrowserRouter basename={ window.location.pathname || '' }>
+  <BrowserRouter basename="/React">
     <App />
   </BrowserRouter>,
   document.getElementById('root'),
